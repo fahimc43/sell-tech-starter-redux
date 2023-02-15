b7b67f4fbaad5fe17ae5db77b88d8ed54041a67b
Fahim Chowdhury <fahimc43@gmail.com>
2023-02-16 04:41:10 +0600

    done

---
+++ src/redux/actionCreater/productAction.js
@@ -0,0 +1,15 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actionTypes/actionTypes";
+
+export const addToCart = (data) => {
+  return {
+    type: ADD_TO_CART,
+    payload: data,
+  };
+};
+
+export const removeFromCart = (data) => {
+  return {
+    type: REMOVE_FROM_CART,
+    payload: data,
+  };
+};
